e04513966b8a9021c24f83b1275ee44959d746fc
Deepak <er.d.ahuja@gmail.com>
2018-07-04 06:42:46 +0530

    fix: update server url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3000';
+const SERVER_URL = process.env.SERVER_URL || 'https://afternoon-spire-79461.herokuapp.com';
 export default {
   SERVER_URL,
 }
